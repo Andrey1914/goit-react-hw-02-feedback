25aee50ed2d664adffcfc8ea9b6ab40346eb819d
Andrey1914 <anlikajw@gmail.com>
2022-07-09 18:40:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,6 @@ import Statistics from './Statistics/Statistics';
 import Container from './Container/Container';
 import Notification from './Notification/Notification';
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101',
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
-
 export default class App extends Component {
   state = {
     good: 0,
